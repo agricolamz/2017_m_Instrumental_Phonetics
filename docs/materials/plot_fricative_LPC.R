24db8f585a491837c80e328608b796b88925b665
agricolamz <agricolamz@gmail.com>
2017-06-10 09:53:13 +0300

    add 4. lecture and materials

---
+++ docs/materials/plot_fricative_LPC.R
@@ -0,0 +1,20 @@
+# set your working directory ----------------------------------------------
+setwd("...")
+
+# read all files in the derictory to R ------------------------------------
+myfiles  <- lapply(list.files(), read.delim)
+
+# merge files into one dataframe ------------------------------------------
+result_df <- Reduce(rbind, myfiles)
+
+# create labels -----------------------------------------------------------
+sounds <- c("l", "sh", "s", "x", "X")
+result_df$label <- rep(sounds, sapply(myfiles, nrow))
+
+# draw the plot -----------------------------------------------------------
+library(ggplot2)
+ggplot(data = result_df,
+       aes(x = freq.Hz., 
+           y = pow.dB.Hz., 
+           color = label))+
+  geom_line(size = 2)
